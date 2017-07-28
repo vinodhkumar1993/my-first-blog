1df40c636954ef958f7f4a09ff51cf4a8c47427b
vinodhkumar1993 <vinodhkumar1993@gmail.com>
2017-07-27 20:42:12 -0500

    Sign-up page Creation

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views
+from django.contrib.auth import login, logout
+from . import views
 urlpatterns = [
+    url(r'^$',views.login_redirect, name='login_redirect'),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-    url(r'', include('blog.urls')),
-]
+    url(r'^account/register/$', views.register, name='register'),
+    url(r'^account/register/complete/$', views.registration_complete, name='registration_complete'),
+    url(r'^account/login/$', views.login, name='login'),
+    url(r'^account/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('blog.urls'))
+    
+    ]
